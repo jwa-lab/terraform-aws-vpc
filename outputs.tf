e3c84d4fd14ce1e2721b4aebc2c17841bd7ba9b8
Thomas Klein <thomas.klein@jwalab.com>
2022-02-23 16:34:47 +0100

    added module outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "public_subnets_ids" {
+  value = [for subnet in aws_subnet.public_subnets : subnet.id]
+}
+
+output "apps_subnets_ids" {
+  value = [for subnet in aws_subnet.apps_subnets : subnet.id]
+}
+
+output "data_subnets_ids" {
+  value = [for subnet in aws_subnet.data_subnets : subnet.id]
+}

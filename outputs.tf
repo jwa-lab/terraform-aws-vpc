69d10106d987e9c9e04c5c9dddb1f75c6fbb3263
Thomas Klein <thomas.klein@jwalab.com>
2023-01-11 10:24:10 +0100

    improve vpc_id output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "vpc_id" {
+  /**
+   * Consumers of the VPC module may read the subnets by using the "aws_subnets" data provider, based on the VPC id.
+   * With the "depends_on" instruction, we can make sure the subnets are available when the id is exported.
+   */
+  depends_on = [
+    aws_subnet.public_subnets,
+    aws_subnet.apps_subnets,
+    aws_subnet.data_subnets,
+  ]
+
   value = aws_vpc.vpc.id
 }
 
